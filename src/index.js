e56a616ab024715f10e4095613c9d987fec5d521
Guy Bridge <guy@bridgeit.com.au>
2018-11-15 07:47:29 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './app.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Header = () =>
+{
+    return(
+    <header>
+        <h1>Scoreboard</h1>
+        <span className="stats">Players: 1</span>
+        </header>
+    );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Player = () =>
+{
+    return(
+        <div className="player">
+        <span className="player-name">
+            Guy
+        </span>
+                <Counter />
+        </div>
+
+    );
+}
+
+const Counter = () =>
+{
+    return(
+    <div className="counter">
+        <button className="counter-action decrement"> - </button>
+        <span className="counter-score">35
+        </span>
+        <button className="counter-action increment"> + </button>
+        </div>
+    );
+}
+
+const App = () =>
+{
+    return(
+     <div class="scoreboard">
+        <Header />
+        
+        { /* Players list */}
+        
+        <Player />
+        </div>
+    );
+}
+
+ReactDOM.render(
+<App />, 
+document.getElementById("root"),
+
+);
