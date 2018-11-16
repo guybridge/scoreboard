789e3c928602383e7822656486dff24b4f40032e
Guy Bridge <guy@bridgeit.com.au>
2018-11-16 08:38:40 +0800

    Added players object

--- src/index.js
+++ src/index.js
@@ -2,56 +2,91 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './app.css';
 
-const Header = () =>
+// Create the players array of objects
+const players = [
+ {
+     name: "Guyb",
+     score: 50,
+	 id: 1
+ },
+ {
+     name: "Guil",
+	 score: 65,
+	  id: 2
+ },
+ {
+ 	 name: "Ben",
+	 score: 26,
+	 id: 3
+	 	
+ },
+ {
+	 name: "John",
+	 score: 77,
+	 id: 4
+ }
+];
+
+const Header = (props) =>
 {
     return(
     <header>
-        <h1>Scoreboard</h1>
-        <span className="stats">Players: 1</span>
+        <h1>{ props.title }</h1>
+        <span className="stats">Players: {props.totalPlayers}</span>
         </header>
     );
 }
 
-const Player = () =>
+const Player = (props) =>
 {
     return(
         <div className="player">
         <span className="player-name">
-            Guy
+            {props.name}
         </span>
-                <Counter />
+                <Counter score={props.score}/>
         </div>
+        
+        
 
     );
 }
 
-const Counter = () =>
+const Counter = (props) =>
 {
     return(
     <div className="counter">
         <button className="counter-action decrement"> - </button>
-        <span className="counter-score">35
+        <span className="counter-score">{props.score}
         </span>
         <button className="counter-action increment"> + </button>
         </div>
     );
 }
 
-const App = () =>
+const App = (props) =>
 {
     return(
-     <div class="scoreboard">
-        <Header />
+     <div className="scoreboard">
+        <Header
+		title="Scoreboard"
+		totalPlayers={props.initialPlayers.length} />
         
         { /* Players list */}
-        
-        <Player />
+        {props.initialPlayers.map(
+			player =>
+		    <Player name={player.name}
+		    score={player.score}
+			key={player.id.toString()}
+		/>
+	    )}
+      
         </div>
     );
 }
 
 ReactDOM.render(
-<App />, 
+<App initialPlayers={players}/>, 
 document.getElementById("root"),
 
 );
